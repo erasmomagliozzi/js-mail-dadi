9866a3296bf3f63a08128498aa6a8b5c04568c6e
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-07 16:05:07 +0100

    wip

---
+++ mail/js/script-mail.js
@@ -0,0 +1,16 @@
+
+
+var mailUtente = prompt('Inserisci la tua mail');
+console.log(mailUtente);
+
+
+var mailDatabase = ['marco@marco.com', 'paolo@paolo.com', 'andrea@andrea.com', 'franco@franco.com'];
+console.log(mailDatabase);
+
+for (var i = 0; i < mailDatabase.length; i++){
+  if (mailUtente == mailDatabase){
+    alert('email presente nel database');
+  }else {
+    alert ('email assente dal database');
+  }
+}
