d8afb51ba4b3267d188f1bebb63f5e367c8c1c17
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-07 17:28:26 +0100

    dadi

--- dadi/js/script-dadi.js
+++ dadi/js/script-dadi.js
@@ -1,14 +1,13 @@
 var giocatori = ['Alessio', 'Angelo'];
 
-
-
 giocatori[0] = Math.floor(Math.random() * 6) + 1;
 console.log(giocatori[0]);
 
 giocatori[1] = Math.floor(Math.random() * 6) + 1;
 console.log(giocatori[1]);
-var angeloVincente = true;
+
+var angeloVincente = 'Ha vinto Angelo';
 if(giocatori[0] > giocatori[1]){
-  angeloVincente = false;
+  angeloVincente = 'Ha vinto Alessio';
 }
 console.log(angeloVincente);
